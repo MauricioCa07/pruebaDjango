58daa2489db19fd487fee2252bcf4f92d47a8039
Edwar Mauricio Carrillo Carvajal <emcarrillc@eafit.edu.co>
2023-08-09 22:20:22 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import Movie
 
 def home (request):
     #return HttpResponse(request,'home.html',{'name':'Greg Lim'})
     #return HttpResponse('<h1>Hola</h1>')
-    return render(request,'home.html',{'name':'Mauricio Carrillo'})
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
 
 def about(request):
     return render(request,'about.html')
