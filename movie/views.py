796745cd9e6099a64aff8a1cd95569d59fbddf70
Edwar Mauricio Carrillo Carvajal <emcarrillc@eafit.edu.co>
2023-08-23 23:12:44 -0500

    segundo trabajo

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 from .models import Movie
 
-def home (request):
-    #return HttpResponse(request,'home.html',{'name':'Greg Lim'})
-    #return HttpResponse('<h1>Hola</h1>')
-    searchTerm = request.GET.get('searchMovie')
-    return render(request,'home.html',{'searchTerm':searchTerm,'movies':searchTerm})
+# Create your views here.
 
-def about(request):
-    return render(request,'about.html')
-    
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
+
+def about():
+    return HttpResponse('<h1>Welcome to About Page</h1>')
--- moviereview/settings.py
+++ moviereview/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'moviereview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +70,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static"),
+    'moviereviews/static/',
+
 ]
 
 WSGI_APPLICATION = 'moviereview.wsgi.application'
